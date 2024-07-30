59d397d4b572f88ee5d4bd1c196f19ec9fda5967
Wasifpatel <140307922+Wasifpatel@users.noreply.github.com>
2024-07-30 18:27:31 +0530

    Delete 12.Bit manupulation directory

--- 12.Bit manupulation/_5ofPower2.java
+++
@@ -1,9 +0,0 @@
-public class _5ofPower2 {
-    public static boolean ofpower2(int n) {
-        return (n & (n-1)) == 0;
-    }
-    public static void main(String[] args) {
-        
-        System.out.println(ofpower2(2));
-    }
-}
